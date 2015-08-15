9c989155cfc7ce1b6d702bae9eb4d1347f443ade
Matthew <matthew@synesis.com.au>
2015-08-15 13:24:58 +1000

    ~ enhancing existing tests

--- clasp/clasp_test.go
+++ clasp/clasp_test.go
@@ -48,14 +48,20 @@ func Test_single_value(t *testing.T) {
 	argument0 := args1.Arguments[0]
 	check(t, "abc" == argument0.Value, "arguments has wrong argument")
 	check(t, 1 == argument0.CmdLineIndex, "arguments has wrong argument")
+
 	check(t, 0 == len(args1.Flags), "arguments object has wrong number of flags")
+
 	check(t, 0 == len(args1.Options), "arguments object has wrong number of options")
+
 	check(t, 1 == len(args1.Values), "arguments object has wrong number of values")
 	value0 := args1.Values[0]
-	check(t, "abc" == value0.Value, "arguments has wrong value")
 	check(t, 1 == value0.CmdLineIndex, "arguments has wrong command-line index")
+	check(t, Value == value0.Type, "argument has wrong type: expected: %v; received %v", Value, value0.Type)
+	check(t, "abc" == value0.Value, "arguments has wrong value")
+
 	check(t, 2 == len(args1.Argv), "arguments object has wrong number of argv members")
 	check(t, case1_argv[0] == args1.Argv[0], "arguments object has wrong executable name")
+
 	check(t, "blah" == args1.ProgramName, "arguments object has wrong program name: has '%s' and should be '%s'", args1.ProgramName, "blah")
 }
 
@@ -89,14 +95,18 @@ func Test_several_values(t *testing.T) {
 	value1 := args1.Values[1]
 	value2 := args1.Values[2]
 	value3 := args1.Values[3]
-	check(t, "abc" == value0.Value, "arguments has wrong value")
 	check(t, 1 == value0.CmdLineIndex, "arguments has wrong command-line index")
-	check(t, "def" == value1.Value, "arguments has wrong value")
+	check(t, Value == value0.Type, "argument has wrong type: expected: %v; received %v", Value, value0.Type)
+	check(t, "abc" == value0.Value, "arguments has wrong value")
 	check(t, 2 == value1.CmdLineIndex, "arguments has wrong command-line index")
-	check(t, "g" == value2.Value, "arguments has wrong value")
+	check(t, Value == value1.Type, "argument has wrong type: expected: %v; received %v", Value, value1.Type)
+	check(t, "def" == value1.Value, "arguments has wrong value")
 	check(t, 3 == value2.CmdLineIndex, "arguments has wrong command-line index")
-	check(t, "hi" == value3.Value, "arguments has wrong value")
+	check(t, Value == value2.Type, "argument has wrong type: expected: %v; received %v", Value, value2.Type)
+	check(t, "g" == value2.Value, "arguments has wrong value")
 	check(t, 4 == value3.CmdLineIndex, "arguments has wrong command-line index")
+	check(t, Value == value3.Type, "argument has wrong type: expected: %v; received %v", Value, value3.Type)
+	check(t, "hi" == value3.Value, "arguments has wrong value")
 
 	check(t, 5 == len(args1.Argv), "arguments object has wrong number of argv members")
 	check(t, case1_argv[0] == args1.Argv[0], "arguments object has wrong executable name")
@@ -138,17 +148,21 @@ func Test_several_values_with_double_hyphen(t *testing.T) {
 	value1 := args1.Values[1]
 	value2 := args1.Values[2]
 	value3 := args1.Values[3]
-	check(t, "abc" == value0.Value, "arguments has wrong value")
 	check(t, 1 == value0.CmdLineIndex, "arguments has wrong command-line index")
+	check(t, Value == value0.Type, "argument has wrong type: expected: %v; received %v", Value, value0.Type)
+	check(t, "abc" == value0.Value, "arguments has wrong value")
 	check(t, 0 == value0.NumGivenHyphens, "arguments has wrong number of hyphens")
-	check(t, "def" == value1.Value, "arguments has wrong value")
 	check(t, 2 == value1.CmdLineIndex, "arguments has wrong command-line index")
+	check(t, Value == value1.Type, "argument has wrong type: expected: %v; received %v", Value, value1.Type)
+	check(t, "def" == value1.Value, "arguments has wrong value")
 	check(t, 0 == value1.NumGivenHyphens, "arguments has wrong number of hyphens")
-	check(t, "-g" == value2.Value, "arguments has wrong value")
 	check(t, 4 == value2.CmdLineIndex, "arguments has wrong command-line index")
+	check(t, Value == value2.Type, "argument has wrong type: expected: %v; received %v", Value, value2.Type)
+	check(t, "-g" == value2.Value, "arguments has wrong value")
 	check(t, 0 == value2.NumGivenHyphens, "arguments has wrong number of hyphens")
-	check(t, "--hi" == value3.Value, "arguments has wrong value")
 	check(t, 5 == value3.CmdLineIndex, "arguments has wrong command-line index")
+	check(t, Value == value3.Type, "argument has wrong type: expected: %v; received %v", Value, value3.Type)
+	check(t, "--hi" == value3.Value, "arguments has wrong value")
 	check(t, 0 == value3.NumGivenHyphens, "arguments has wrong number of hyphens")
 
 	check(t, 6 == len(args1.Argv), "arguments object has wrong number of argv members")
