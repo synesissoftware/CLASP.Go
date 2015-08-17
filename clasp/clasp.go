58b90e33e970395902241324549e494e03e5cd2b
Matthew <matthew@synesis.com.au>
2015-08-17 14:18:19 +1000

    ~ polishing

--- clasp/clasp.go
+++ clasp/clasp.go
@@ -1,17 +1,67 @@
+/* /////////////////////////////////////////////////////////////////////////
+ * File:        clasp/clasp.go
+ *
+ * Purpose:     CLASP library in Go
+ *
+ * Created:     15th August 2015
+ * Updated:     17th August 2015
+ *
+ * Home:        http://synesis.com.au/software
+ *
+ * Copyright (c) 2015, Matthew Wilson
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - Neither the names of Matthew Wilson and Synesis Software nor the names
+ *   of any contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ////////////////////////////////////////////////////////////////////// */
+
 
 package clasp
 
 import (
-	"fmt"
 	"path"
 	"strings"
 )
+
+const (
+)
+
+/* /////////////////////////////////////////////////////////////////////////
+ * constants
+ */
 
 type ParseFlag int
 
 const (
 	ParseTreatSingleHyphenAsValue ParseFlag = 1 << iota
 )
+
+/* /////////////////////////////////////////////////////////////////////////
+ * types
+ */
 
 type ArgType int
 
@@ -55,6 +105,10 @@ type ParseParams struct {
 	Flags		ParseFlag
 }
 
+/* /////////////////////////////////////////////////////////////////////////
+ * helpers
+ */
+
 func (params ParseParams) findAlias(name string) (found bool, alias Alias) {
 
 	// Algorithm:
@@ -81,6 +135,10 @@ func (params ParseParams) findAlias(name string) (found bool, alias Alias) {
 
 	return false, dummy
 }
+
+/* /////////////////////////////////////////////////////////////////////////
+ * API
+ */
 
 func Parse(argv []string, params ParseParams) *Arguments {
 	args := new(Arguments)
@@ -110,6 +168,7 @@ func Parse(argv []string, params ParseParams) *Arguments {
 
 		arg := new(Argument)
 
+
 		arg.CmdLineIndex	=	i + 1
 		arg.Flags			=	int(params.Flags)
 		arg.AliasIndex		=	-1
@@ -125,6 +184,7 @@ func Parse(argv []string, params ParseParams) *Arguments {
 				numHyphens	=	strings.IndexFunc(s, func(c rune) bool { return '-' != c })
 			}
 		}
+
 
 		arg.NumGivenHyphens	=	numHyphens
 
@@ -185,4 +245,6 @@ func Parse(argv []string, params ParseParams) *Arguments {
 
 	return args
 }
+
+/* ///////////////////////////// end of file //////////////////////////// */
 
