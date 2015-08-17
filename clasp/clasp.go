74fed34efd801ab695c104d912851122dbe0a9ae
Matthew <matthew@synesis.com.au>
2015-08-17 14:22:53 +1000

    + added version information

--- clasp/clasp.go
+++ clasp/clasp.go
@@ -47,6 +47,10 @@ import (
 )
 
 const (
+	VersionMajor int16		=	0
+	VersionMinor int16		=	1
+	VersionRevision int16	=	1
+	Version int64			=	(int64(VersionMajor) << 48) + (int64(VersionMinor) << 32) + (int64(VersionRevision) << 16)
 )
 
 /* /////////////////////////////////////////////////////////////////////////
