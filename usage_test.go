4f16b0eef171968119ba7b71d06243f0c7125bde
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-02-28 07:07:47 +1100

    ~ changed some tests to exercise the ability of `Version` to be array of numbers

--- usage_test.go
+++ usage_test.go
@@ -1,8 +1,6 @@
-
 package clasp_test
 
 import (
-
 	angols "github.com/synesissoftware/ANGoLS"
 	clasp "github.com/synesissoftware/CLASP.Go"
 
@@ -12,7 +10,6 @@ import (
 	"strings"
 	"testing"
 )
-
 
 func check_num_lines(t *testing.T, result []string, num_lines int) {
 
@@ -63,14 +60,14 @@ func call_ShowUsage_(t *testing.T, specifications []clasp.Specification, ups cla
 
 	t.Helper()
 
-	buf		:=	new(bytes.Buffer)
+	buf := new(bytes.Buffer)
 
 	ups.Stream = buf
 	ups.UsageFlags |= clasp.DontCallExit
 
-	var xc	int;
+	var xc int
 
-	xc, err		=	clasp.ShowUsage(specifications, ups)
+	xc, err = clasp.ShowUsage(specifications, ups)
 	if err != nil {
 
 		t.Errorf("ShowUsage() failed with exit code: %d", xc)
@@ -94,20 +91,20 @@ func test_ShowVersion_(t *testing.T, expected string, specifications []clasp.Spe
 
 	t.Helper()
 
-	buf			:=	new(bytes.Buffer)
-	params		:=	clasp.UsageParams{
+	buf := new(bytes.Buffer)
+	params := clasp.UsageParams{
 
-		Stream: buf,
-		ProgramName: program_name,
-		UsageFlags: clasp.DontCallExit,
-		ValuesString: "",
-		ExitCode: 0,
-		Exiter: nil,
-		Version: version,
+		Stream:        buf,
+		ProgramName:   program_name,
+		UsageFlags:    clasp.DontCallExit,
+		ValuesString:  "",
+		ExitCode:      0,
+		Exiter:        nil,
+		Version:       version,
 		VersionPrefix: version_prefix,
 	}
 
-	xc, err		:=	clasp.ShowVersion(specifications, params)
+	xc, err := clasp.ShowVersion(specifications, params)
 	if err != nil {
 
 		t.Errorf("ShowVersion() failed with exit code: %d", xc)
@@ -118,7 +115,7 @@ func test_ShowVersion_(t *testing.T, expected string, specifications []clasp.Spe
 			t.Error("return code is not 0")
 		} else {
 
-			actual		:=	buf.String()
+			actual := buf.String()
 
 			if expected != actual {
 
@@ -130,25 +127,34 @@ func test_ShowVersion_(t *testing.T, expected string, specifications []clasp.Spe
 
 func Test_ShowVersion_with_int_array_version(t *testing.T) {
 
-	var specifications		[]clasp.Specification
+	var specifications []clasp.Specification
 
-	test_ShowVersion_(t, "myprog 1.2.3\n", specifications, "myprog", []int{ 1, 2, 3 }, "")
+	test_ShowVersion_(t, "myprog 1.2.3\n", specifications, "myprog", []int{1, 2, 3}, "")
 
-	test_ShowVersion_(t, "myprog v1.2.3\n", specifications, "myprog", []int{ 1, 2, 3 }, "v")
+	test_ShowVersion_(t, "myprog v1.2.3\n", specifications, "myprog", []int{1, 2, 3}, "v")
+}
+
+func Test_ShowVersion_with_uint16_array_version(t *testing.T) {
+
+	var specifications []clasp.Specification
+
+	test_ShowVersion_(t, "myprog 1.2.3\n", specifications, "myprog", []uint16{1, 2, 3}, "")
+
+	test_ShowVersion_(t, "myprog v1.2.3\n", specifications, "myprog", []uint16{1, 2, 3}, "v")
 }
 
 func Test_ShowVersion_with_string_array_version(t *testing.T) {
 
-	var specifications		[]clasp.Specification
+	var specifications []clasp.Specification
 
-	test_ShowVersion_(t, "myprog 0.1.9\n", specifications, "myprog", []string{ "0", "1", "9" }, "")
+	test_ShowVersion_(t, "myprog 0.1.9\n", specifications, "myprog", []string{"0", "1", "9"}, "")
 
-	test_ShowVersion_(t, "myprog v0.1.9\n", specifications, "myprog", []string{ "0", "1", "9" }, "v")
+	test_ShowVersion_(t, "myprog v0.1.9\n", specifications, "myprog", []string{"0", "1", "9"}, "v")
 }
 
 func Test_ShowVersion_with_string_version(t *testing.T) {
 
-	var specifications		[]clasp.Specification
+	var specifications []clasp.Specification
 
 	test_ShowVersion_(t, "myprog 0.1.9\n", specifications, "myprog", "0.1.9", "")
 
@@ -157,35 +163,36 @@ func Test_ShowVersion_with_string_version(t *testing.T) {
 
 func Test_ShowVersion_with_inferred_ProgramName(t *testing.T) {
 
-	var specifications		[]clasp.Specification
+	var specifications []clasp.Specification
 
-	program_name	:=	os.Args[0]
-	program_name	=	path.Base(program_name)
+	program_name := os.Args[0]
+	program_name = path.Base(program_name)
 
-	test_ShowVersion_(t, program_name + " 0.1.2\n", specifications, "", "0.1.2", "")
+	test_ShowVersion_(t, program_name+" 0.1.2\n", specifications, "", "0.1.2", "")
 }
 
 func Test_ShowUsage_1(t *testing.T) {
 
-	var	specifications					[]clasp.Specification
-	values_string				:=	""
-	flags_and_options_string	:=	""
-	info_lines					:=	make([]string, 0)
+	var specifications []clasp.Specification
+	values_string := ""
+	flags_and_options_string := ""
+	info_lines := make([]string, 0)
 
-	usage_params_base			:=	clasp.UsageParams {
+	usage_params_base := clasp.UsageParams{
 
-		ProgramName:			"myprogram",
-		VersionPrefix:			"v",
-		Version:				"0.1.2",
-		ExitCode:				0,
-		ValuesString:			values_string,
-		FlagsAndOptionsString:	flags_and_options_string,
-		InfoLines:				info_lines,
+		ProgramName:           "myprogram",
+		VersionPrefix:         "v",
+		Version:               "0.1.2",
+		ExitCode:              0,
+		ValuesString:          values_string,
+		FlagsAndOptionsString: flags_and_options_string,
+		InfoLines:             info_lines,
 	}
 
-	result, err					:=	call_ShowUsage_(t, specifications, usage_params_base)
+	result, err := call_ShowUsage_(t, specifications, usage_params_base)
 	if err != nil {
 
+		t.Fail()
 	} else {
 
 		check_num_nonblank_lines(t, result, 1)
@@ -197,10 +204,10 @@ func Test_ShowUsage_1(t *testing.T) {
 
 func Test_ShowUsage_2(t *testing.T) {
 
-	var	specifications					[]clasp.Specification
-	values_string				:=	""
-	flags_and_options_string	:=	""
-	info_lines					:=	[]string {
+	var specifications []clasp.Specification
+	values_string := ""
+	flags_and_options_string := ""
+	info_lines := []string{
 
 		"CLASP.Go Test Suite",
 		"",
@@ -208,20 +215,21 @@ func Test_ShowUsage_2(t *testing.T) {
 		"",
 	}
 
-	usage_params_base			:=	clasp.UsageParams {
+	usage_params_base := clasp.UsageParams{
 
-		ProgramName:			"myprogram",
-		VersionPrefix:			"V",
-		Version:				"0.1.2",
-		ExitCode:				0,
-		ValuesString:			values_string,
-		FlagsAndOptionsString:	flags_and_options_string,
-		InfoLines:				info_lines,
+		ProgramName:           "myprogram",
+		VersionPrefix:         "V",
+		Version:               []int{0, 1, 2},
+		ExitCode:              0,
+		ValuesString:          values_string,
+		FlagsAndOptionsString: flags_and_options_string,
+		InfoLines:             info_lines,
 	}
 
-	result, err					:=	call_ShowUsage_(t, specifications, usage_params_base)
+	result, err := call_ShowUsage_(t, specifications, usage_params_base)
 	if err != nil {
 
+		t.Fail()
 	} else {
 
 		check_num_nonblank_lines(t, result, 3)
@@ -237,28 +245,29 @@ func Test_ShowUsage_2(t *testing.T) {
 
 func Test_ShowUsage_3_a(t *testing.T) {
 
-	specifications				:=	[]clasp.Specification{
+	specifications := []clasp.Specification{
 
 		clasp.Flag("--high").SetAlias("-h").SetHelp("makes things high"),
 	}
-	values_string				:=	""
-	flags_and_options_string	:=	""
-	info_lines					:=	make([]string, 0)
+	values_string := ""
+	flags_and_options_string := ""
+	info_lines := make([]string, 0)
 
-	usage_params_base			:=	clasp.UsageParams {
+	usage_params_base := clasp.UsageParams{
 
-		ProgramName:			"myprogram",
-		VersionPrefix:			"v",
-		Version:				"0.1.2",
-		ExitCode:				0,
-		ValuesString:			values_string,
-		FlagsAndOptionsString:	flags_and_options_string,
-		InfoLines:				info_lines,
+		ProgramName:           "myprogram",
+		VersionPrefix:         "v",
+		Version:               "0.1.2",
+		ExitCode:              0,
+		ValuesString:          values_string,
+		FlagsAndOptionsString: flags_and_options_string,
+		InfoLines:             info_lines,
 	}
 
-	result, err					:=	call_ShowUsage_(t, specifications, usage_params_base)
+	result, err := call_ShowUsage_(t, specifications, usage_params_base)
 	if err != nil {
 
+		t.Fail()
 	} else {
 
 		check_num_nonblank_lines(t, result, 5)
@@ -274,28 +283,29 @@ func Test_ShowUsage_3_a(t *testing.T) {
 
 func Test_ShowUsage_3_b(t *testing.T) {
 
-	specifications				:=	[]clasp.Specification{
+	specifications := []clasp.Specification{
 
 		clasp.Flag("--high").SetAlias("-h").SetHelp("makes things high"),
 	}
-	values_string				:=	""
-	flags_and_options_string	:=	" "
-	info_lines					:=	make([]string, 0)
+	values_string := ""
+	flags_and_options_string := " "
+	info_lines := make([]string, 0)
 
-	usage_params_base			:=	clasp.UsageParams {
+	usage_params_base := clasp.UsageParams{
 
-		ProgramName:			"myprogram",
-		VersionPrefix:			"v",
-		Version:				"0.1.2",
-		ExitCode:				0,
-		ValuesString:			values_string,
-		FlagsAndOptionsString:	flags_and_options_string,
-		InfoLines:				info_lines,
+		ProgramName:           "myprogram",
+		VersionPrefix:         "v",
+		Version:               "0.1.2",
+		ExitCode:              0,
+		ValuesString:          values_string,
+		FlagsAndOptionsString: flags_and_options_string,
+		InfoLines:             info_lines,
 	}
 
-	result, err					:=	call_ShowUsage_(t, specifications, usage_params_base)
+	result, err := call_ShowUsage_(t, specifications, usage_params_base)
 	if err != nil {
 
+		t.Fail()
 	} else {
 
 		check_num_nonblank_lines(t, result, 5)
@@ -311,28 +321,29 @@ func Test_ShowUsage_3_b(t *testing.T) {
 
 func Test_ShowUsage_3_c(t *testing.T) {
 
-	specifications				:=	[]clasp.Specification{
+	specifications := []clasp.Specification{
 
 		clasp.Flag("--high").SetAlias("-h").SetHelp("makes things high"),
 	}
-	values_string				:=	""
-	flags_and_options_string	:=	"[ flags/options ]"
-	info_lines					:=	make([]string, 0)
+	values_string := ""
+	flags_and_options_string := "[ flags/options ]"
+	info_lines := make([]string, 0)
 
-	usage_params_base			:=	clasp.UsageParams {
+	usage_params_base := clasp.UsageParams{
 
-		ProgramName:			"myprogram",
-		VersionPrefix:			"v",
-		Version:				"0.1.2",
-		ExitCode:				0,
-		ValuesString:			values_string,
-		FlagsAndOptionsString:	flags_and_options_string,
-		InfoLines:				info_lines,
+		ProgramName:           "myprogram",
+		VersionPrefix:         "v",
+		Version:               "0.1.2",
+		ExitCode:              0,
+		ValuesString:          values_string,
+		FlagsAndOptionsString: flags_and_options_string,
+		InfoLines:             info_lines,
 	}
 
-	result, err					:=	call_ShowUsage_(t, specifications, usage_params_base)
+	result, err := call_ShowUsage_(t, specifications, usage_params_base)
 	if err != nil {
 
+		t.Fail()
 	} else {
 
 		check_num_nonblank_lines(t, result, 5)
@@ -348,29 +359,30 @@ func Test_ShowUsage_3_c(t *testing.T) {
 
 func Test_ShowUsage_4(t *testing.T) {
 
-	specifications				:=	[]clasp.Specification{
+	specifications := []clasp.Specification{
 
 		clasp.Option("--verbosity").SetHelp("Specifies the verbosity").SetValues("low", "medium", "high"),
 		clasp.Flag("--verbosity=high").SetAlias("-v"),
 	}
-	values_string				:=	""
-	flags_and_options_string	:=	"[ flags/options ]"
-	info_lines					:=	make([]string, 0)
+	values_string := ""
+	flags_and_options_string := "[ flags/options ]"
+	info_lines := make([]string, 0)
 
-	usage_params_base			:=	clasp.UsageParams {
+	usage_params_base := clasp.UsageParams{
 
-		ProgramName:			"myprogram",
-		VersionPrefix:			"v",
-		Version:				"0.1.2",
-		ExitCode:				0,
-		ValuesString:			values_string,
-		FlagsAndOptionsString:	flags_and_options_string,
-		InfoLines:				info_lines,
+		ProgramName:           "myprogram",
+		VersionPrefix:         "v",
+		Version:               []int{0, 1, 2},
+		ExitCode:              0,
+		ValuesString:          values_string,
+		FlagsAndOptionsString: flags_and_options_string,
+		InfoLines:             info_lines,
 	}
 
-	result, err					:=	call_ShowUsage_(t, specifications, usage_params_base)
+	result, err := call_ShowUsage_(t, specifications, usage_params_base)
 	if err != nil {
 
+		t.Fail()
 	} else {
 
 		check_num_nonblank_lines(t, result, 9)
@@ -388,83 +400,93 @@ func Test_ShowUsage_4(t *testing.T) {
 	}
 }
 
-
 func Test_ShowUsage_5_a(t *testing.T) {
 
-	specifications				:=	[]clasp.Specification{
+	specifications := []clasp.Specification{
 
 		clasp.Section("verbosity:"),
 		clasp.Option("--verbosity").SetHelp("Specifies the verbosity").SetValues("low", "medium", "high"),
 		clasp.Flag("--verbosity=high").SetAlias("-v"),
 		clasp.Flag("--debug"),
 	}
-	values_string				:=	""
-	flags_and_options_string	:=	"[ flags/options ]"
-	info_lines					:=	make([]string, 0)
+	values_string := ""
+	flags_and_options_string := "[ flags/options ]"
+	info_lines := []string{"CLASP.Go Examples", "", ":version:", ""}
 
-	usage_params_base			:=	clasp.UsageParams {
+	usage_params_base := clasp.UsageParams{
 
-		ProgramName:			"myprogram",
-		VersionPrefix:			"v",
-		Version:				"0.1.2",
-		ExitCode:				0,
-		ValuesString:			values_string,
-		FlagsAndOptionsString:	flags_and_options_string,
-		InfoLines:				info_lines,
-		UsageFlags:				0,
+		ProgramName:           "myprogram",
+		VersionPrefix:         "v",
+		Version:               "0.1.2",
+		ExitCode:              0,
+		ValuesString:          values_string,
+		FlagsAndOptionsString: flags_and_options_string,
+		InfoLines:             info_lines,
+		UsageFlags:            0,
 	}
 
-	result, err					:=	call_ShowUsage_(t, specifications, usage_params_base)
+	result, err := call_ShowUsage_(t, specifications, usage_params_base)
 	if err != nil {
 
+		t.Fail()
 	} else {
 
-		check_num_nonblank_lines(t, result, 11)
-		check_num_lines(t, result, 17)
+		check_num_nonblank_lines(t, result, 13)
+		check_num_lines(t, result, 21)
 
-		check_line_equal(t, result[0], "USAGE: myprogram [ flags/options ]")
-		check_line_equal(t, result[2], "flags/options:")
-		check_stripped_line_equal(t, result[4], "verbosity:")
-		check_stripped_line_equal(t, result[6], "-v --verbosity=high")
-		check_stripped_line_equal(t, result[7], "--verbosity=<value>")
-		check_stripped_line_equal(t, result[8], "Specifies the verbosity")
-		check_stripped_line_equal(t, result[9], "where <value> one of:")
-		check_stripped_line_equal(t, result[10], "low")
-		check_stripped_line_equal(t, result[11], "medium")
-		check_stripped_line_equal(t, result[12], "high")
-		check_stripped_line_equal(t, result[14], "--debug")
+		check_line_equal(t, result[0], "CLASP.Go Examples")
+		check_line_equal(t, result[1], "")
+		check_line_equal(t, result[2], "myprogram v0.1.2")
+		check_line_equal(t, result[3], "")
+		check_line_equal(t, result[4], "USAGE: myprogram [ flags/options ]")
+		check_line_equal(t, result[5], "")
+		check_line_equal(t, result[6], "flags/options:")
+		check_line_equal(t, result[7], "")
+		check_stripped_line_equal(t, result[8], "verbosity:")
+		check_line_equal(t, result[9], "")
+		check_stripped_line_equal(t, result[10], "-v --verbosity=high")
+		check_stripped_line_equal(t, result[11], "--verbosity=<value>")
+		check_stripped_line_equal(t, result[12], "Specifies the verbosity")
+		check_stripped_line_equal(t, result[13], "where <value> one of:")
+		check_stripped_line_equal(t, result[14], "low")
+		check_stripped_line_equal(t, result[15], "medium")
+		check_stripped_line_equal(t, result[16], "high")
+		check_line_equal(t, result[17], "")
+		check_stripped_line_equal(t, result[18], "--debug")
+		check_line_equal(t, result[19], "")
+		check_line_equal(t, result[20], "")
 	}
 }
 
-
 func Test_ShowUsage_5_b(t *testing.T) {
 
-	specifications				:=	[]clasp.Specification{
+	specifications := []clasp.Specification{
 
 		clasp.Section("verbosity:"),
 		clasp.Option("--verbosity").SetHelp("Specifies the verbosity").SetValues("low", "medium", "high"),
 		clasp.Flag("--verbosity=high").SetAlias("-v"),
 		clasp.Flag("--debug"),
 	}
-	values_string				:=	""
-	flags_and_options_string	:=	"[ flags/options ]"
-	info_lines					:=	make([]string, 0)
+	values_string := ""
+	flags_and_options_string := "[ flags/options ]"
+	info_lines := make([]string, 0)
 
-	usage_params_base			:=	clasp.UsageParams {
+	usage_params_base := clasp.UsageParams{
 
-		ProgramName:			"myprogram",
-		VersionPrefix:			"v",
-		Version:				"0.1.2",
-		ExitCode:				0,
-		ValuesString:			values_string,
-		FlagsAndOptionsString:	flags_and_options_string,
-		InfoLines:				info_lines,
-		UsageFlags:				clasp.SkipBlanksBetweenLines,
+		ProgramName:           "myprogram",
+		VersionPrefix:         "v",
+		Version:               "0.1.2",
+		ExitCode:              0,
+		ValuesString:          values_string,
+		FlagsAndOptionsString: flags_and_options_string,
+		InfoLines:             info_lines,
+		UsageFlags:            clasp.SkipBlanksBetweenLines,
 	}
 
-	result, err					:=	call_ShowUsage_(t, specifications, usage_params_base)
+	result, err := call_ShowUsage_(t, specifications, usage_params_base)
 	if err != nil {
 
+		t.Fail()
 	} else {
 
 		check_num_nonblank_lines(t, result, 11)
@@ -483,4 +505,3 @@ func Test_ShowUsage_5_b(t *testing.T) {
 		check_stripped_line_equal(t, result[13], "--debug")
 	}
 }
-
