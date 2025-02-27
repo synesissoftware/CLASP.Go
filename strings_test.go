6982b656f976f4922c1ed02abc48b6575004eaab
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-02-28 08:00:45 +1100

    ~ boilerplate ; ~ formatting ;

--- strings_test.go
+++ strings_test.go
@@ -1,8 +1,6 @@
-
 package clasp_test
 
 import (
-
 	clasp "github.com/synesissoftware/CLASP.Go"
 
 	"testing"
@@ -12,10 +10,10 @@ import (
 
 func Test_String_of_ArgType_Flag(t *testing.T) {
 
-	at_F		:=	clasp.FlagType
+	at_F := clasp.FlagType
 
-	expected	:=	"Flag"
-	actual		:=	at_F.String()
+	expected := "Flag"
+	actual := at_F.String()
 
 	if expected != actual {
 
@@ -25,10 +23,10 @@ func Test_String_of_ArgType_Flag(t *testing.T) {
 
 func Test_String_of_ArgType_Option(t *testing.T) {
 
-	at_F		:=	clasp.OptionType
+	at_F := clasp.OptionType
 
-	expected	:=	"Option"
-	actual		:=	at_F.String()
+	expected := "Option"
+	actual := at_F.String()
 
 	if expected != actual {
 
@@ -38,10 +36,10 @@ func Test_String_of_ArgType_Option(t *testing.T) {
 
 func Test_String_of_ArgType_Value(t *testing.T) {
 
-	at_F		:=	clasp.ValueType
+	at_F := clasp.ValueType
 
-	expected	:=	"Value"
-	actual		:=	at_F.String()
+	expected := "Value"
+	actual := at_F.String()
 
 	if expected != actual {
 
@@ -53,8 +51,8 @@ func Test_String_of_ArgType_unknown(t *testing.T) {
 
 	var at_F clasp.ArgType = 101
 
-	expected	:=	"<clasp.ArgType 101>"
-	actual		:=	at_F.String()
+	expected := "<clasp.ArgType 101>"
+	actual := at_F.String()
 
 	if expected != actual {
 
@@ -62,15 +60,14 @@ func Test_String_of_ArgType_unknown(t *testing.T) {
 	}
 }
 
-
 // Specification
 
 func Test_String_of_Specification_1(t *testing.T) {
 
-	specification		:=	clasp.Specification{}
+	specification := clasp.Specification{}
 
-	expected	:=	"<clasp.Specification{ Type=<clasp.ArgType 0>, Name=\"\", Aliases=[], Help=\"\", ValueSet=[], BitFlags=0x0, Extras=map[] }>"
-	actual		:=	specification.String()
+	expected := "<clasp.Specification{ Type=<clasp.ArgType 0>, Name=\"\", Aliases=[], Help=\"\", ValueSet=[], BitFlags=0x0, Extras=map[] }>"
+	actual := specification.String()
 
 	if expected != actual {
 
@@ -80,87 +77,82 @@ func Test_String_of_Specification_1(t *testing.T) {
 
 func Test_String_of_Specification_2(t *testing.T) {
 
-	specification		:=	clasp.Specification{ Help: "help, plz", BitFlags: 0x1234, Name: "--flagpole", Type: clasp.OptionType }
+	specification := clasp.Specification{Help: "help, plz", BitFlags: 0x1234, Name: "--flagpole", Type: clasp.OptionType}
 
-	expected	:=	"<clasp.Specification{ Type=Option, Name=\"--flagpole\", Aliases=[], Help=\"help, plz\", ValueSet=[], BitFlags=0x1234, Extras=map[] }>"
-	actual		:=	specification.String()
+	expected := "<clasp.Specification{ Type=Option, Name=\"--flagpole\", Aliases=[], Help=\"help, plz\", ValueSet=[], BitFlags=0x1234, Extras=map[] }>"
+	actual := specification.String()
 
 	if expected != actual {
 
 		t.Errorf("expected '%s' does not equal '%s'", expected, actual)
 	}
 }
-
 
 // Argument
 
 func Test_String_of_Argument_1(t *testing.T) {
 
-	argument	:=	clasp.Argument{
+	argument := clasp.Argument{
 
-		ResolvedName: "--help",
-		GivenName: "--help",
-		Value: "",
-		Type: clasp.FlagType,
-		CmdLineIndex: 1,
+		ResolvedName:    "--help",
+		GivenName:       "--help",
+		Value:           "",
+		Type:            clasp.FlagType,
+		CmdLineIndex:    1,
 		NumGivenHyphens: 2,
-		Flags: 0x1234,
+		Flags:           0x1234,
 	}
 
-	expected	:=	"<clasp.Argument{ ResolvedName=\"--help\", GivenName=\"--help\", Value=\"\", Type=Flag, CmdLineIndex=1, NumGivenHyphens=2, ArgumentSpecification=<nil>, Flags=0x1234, used=false }>"
-	actual		:=	argument.String()
+	expected := "<clasp.Argument{ ResolvedName=\"--help\", GivenName=\"--help\", Value=\"\", Type=Flag, CmdLineIndex=1, NumGivenHyphens=2, ArgumentSpecification=<nil>, Flags=0x1234, used=false }>"
+	actual := argument.String()
 
 	if expected != actual {
 
 		t.Errorf("expected '%s' does not equal '%s'", expected, actual)
 	}
 }
-
 
 // Arguments
 
 func Test_String_of_Arguments_1(t *testing.T) {
 
-	arguments	:=	clasp.Arguments{}
+	arguments := clasp.Arguments{}
 
-	expected	:=	"<clasp.Arguments{ Arguments=[], Flags=[], Options=[], Values=[], Argv=[], ProgramName=\"\" }>"
-	actual		:=	arguments.String()
+	expected := "<clasp.Arguments{ Arguments=[], Flags=[], Options=[], Values=[], Argv=[], ProgramName=\"\" }>"
+	actual := arguments.String()
 
 	if expected != actual {
 
 		t.Errorf("expected '%s' does not equal '%s'", expected, actual)
 	}
 }
-
 
 // ParseParams
 
 func Test_String_of_ParseParams_1(t *testing.T) {
 
-	parseParams	:=	clasp.ParseParams{}
+	parseParams := clasp.ParseParams{}
 
-	expected	:=	"<clasp.ParseParams{ Specifications=[], Flags=0x0 }>"
-	actual		:=	parseParams.String()
+	expected := "<clasp.ParseParams{ Specifications=[], Flags=0x0 }>"
+	actual := parseParams.String()
 
 	if expected != actual {
 
 		t.Errorf("expected '%s' does not equal '%s'", expected, actual)
 	}
 }
-
 
 // UsageParams
 
 func Test_String_of_UsageParams_1(t *testing.T) {
 
-	parseParams	:=	clasp.UsageParams{}
+	parseParams := clasp.UsageParams{}
 
-	expected	:=	"<clasp.UsageParams{ Stream=<nil>, ProgramName=\"\", UsageFlags=0x0, ExitCode=0, Exiter=<nil>, Version=<nil>, VersionPrefix=\"\", InfoLines=[], ValuesString=\"\", FlagsAndOptionsString=\"\" }>"
-	actual		:=	parseParams.String()
+	expected := "<clasp.UsageParams{ Stream=<nil>, ProgramName=\"\", UsageFlags=0x0, ExitCode=0, Exiter=<nil>, Version=<nil>, VersionPrefix=\"\", InfoLines=[], ValuesString=\"\", FlagsAndOptionsString=\"\" }>"
+	actual := parseParams.String()
 
 	if expected != actual {
 
 		t.Errorf("expected '%s' does not equal '%s'", expected, actual)
 	}
 }
-
--- usage.go
+++ usage.go
@@ -5,7 +5,7 @@
 
 /*
  * Created: 4th September 2015
- * Updated: 26th February 2025
+ * Updated: 28th February 2025
  */
 
 package clasp
