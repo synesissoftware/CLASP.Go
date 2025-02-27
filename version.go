e9aa27306649d8b7928aa021c09e073ea092c1e7
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-02-28 09:28:44 +1100

    ~ boilerplate

--- version.go
+++ version.go
@@ -15,8 +15,8 @@ import "github.com/synesissoftware/ver2go"
 const (
 	VersionMajor uint16 = 0
 	VersionMinor uint16 = 16
-	VersionPatch uint16 = 2
-	VersionAB    uint16 = 0xFFFF
+	VersionPatch uint16 = 3
+	VersionAB    uint16 = 0x4001
 	Version      uint64 = (uint64(VersionMajor) << 48) + (uint64(VersionMinor) << 32) + (uint64(VersionPatch) << 16) + (uint64(VersionAB) << 0)
 )
 
