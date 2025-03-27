67ed7a06abd36ce7021c5a204d4df320ff2b1e3c
Matt Wilson <152443343+mwsis@users.noreply.github.com>
2025-03-27 16:10:05 +1100

    0.17 (#10)

--- go.mod
+++ go.mod
@@ -2,14 +2,15 @@ module github.com/synesissoftware/CLASP.Go
 
 go 1.23.6
 
-require github.com/synesissoftware/ANGoLS v0.0.0-20190330004400-955d82dbf73b
+require (
+	github.com/stretchr/testify v1.10.0
+	github.com/synesissoftware/ANGoLS v0.0.0-20190330004400-955d82dbf73b
+	github.com/synesissoftware/STEGoL v0.2.0
+	github.com/synesissoftware/ver2go v0.0.0-alpha3
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
-	github.com/synesissoftware/Diagnosticism.Go v0.6.1 // indirect
-	github.com/synesissoftware/STEGoL v0.2.0 // indirect
-	github.com/synesissoftware/ver2go v0.0.0-alpha3 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- version_test.go
+++ version_test.go
@@ -1,7 +1,8 @@
-package clasp
+package clasp_test
 
 import (
 	"github.com/stretchr/testify/require"
+	. "github.com/synesissoftware/CLASP.Go"
 
 	"testing"
 )
@@ -10,7 +11,7 @@ const (
 	Expected_VersionMajor uint16 = 0
 	Expected_VersionMinor uint16 = 17
 	Expected_VersionPatch uint16 = 0
-	Expected_VersionAB    uint16 = 0x4002
+	Expected_VersionAB    uint16 = 0xFFFF
 )
 
 func Test_Version_Elements(t *testing.T) {
@@ -21,9 +22,9 @@ func Test_Version_Elements(t *testing.T) {
 }
 
 func Test_Version(t *testing.T) {
-	require.Equal(t, uint64(0x0000_0011_0000_4002), Version)
+	require.Equal(t, uint64(0x0000_0011_0000_FFFF), Version)
 }
 
 func Test_Version_String(t *testing.T) {
-	require.Equal(t, "0.17.0-alpha2", VersionString())
+	require.Equal(t, "0.17.0", VersionString())
 }
